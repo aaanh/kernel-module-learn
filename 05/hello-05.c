86ec7701e7a48c3888d73aaa461a610fda5e136d
HA <iam@hoanganh.tech>
2022-01-02 23:24:43 -0500

    finished part 05

--- 05/hello-05.c
+++ 05/hello-05.c
@@ -36,3 +36,41 @@ MODULE_PARM_DESC(mylong, "A long integer.");
 
 module_param(mystring, charp, 0000);
 MODULE_PARM_DESC(mystring, "A character string.");
+
+/**
+ * module_param_array(name, type, num, perm);
+ * The first param is the parameter's (in this case the array's) name.
+ * The second param is the data type of the elements of the array.
+ * The third argument is a pointer to the variable that will store the number
+ * of the elements of the array initialized by the user at module loading time.
+ * The fourth argument is the permission bits.
+ */
+module_param_array(myintarray, int, &arr_argc, 0000);
+MODULE_PARM_DESC(myintarray, "An array of integers.");
+
+static int __init hello_5_init(void)
+{
+    int i;
+
+    pr_info("Hello, world 5\n===========\n");
+    pr_info("myshort is a short integer: %hd\n", myshort);
+    pr_info("myint is an integer: %d\n", myint);
+    pr_info("mylong is a log integer: %ld\n", mylong);
+    pr_info("mystring is a string: %s\n", mystring);
+
+    for (i = 0; i < ARRAY_SIZE(myintarray); i++)
+    {
+        pr_info("myintarray[%d] = %d\n", i, myintarray[i]);
+    }
+    
+    pr_info("got %d arguments for myintarray.\n", arr_argc);
+    return 0;
+}
+
+static void __exit hello_5_exit(void)
+{
+    pr_info("Goodbye world 5!\n");
+}
+
+module_init(hello_5_init);
+module_exit(hello_5_exit);
